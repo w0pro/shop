834fc7adcf67a9e64015b0fd8fd5d466f1e2818c
w0pro <ya.sapronov@gmail.com>
2022-07-13 23:18:02 +0300

    init:start project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <shop-window />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+
+import shopWindow from '@/components/Shop-Window.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    shopWindow,
   },
 };
 </script>
